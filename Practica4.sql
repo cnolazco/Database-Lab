e979cf7a6c57a4ce87f1ae35fcbaf546a596d250
Carolina Nolazco <56035491+cnolazco@users.noreply.github.com>
2020-06-04 22:20:41 -0500

    Create Practica4.sql

---
+++ Practica4.sql
@@ -0,0 +1,84 @@
+-- PRACTICA 4 --
+
+-- INSERT  CLIENTES
+INSERT INTO Clientes VALUES ('Diana Carolina', 'Nolazco', 'Barajas', '2001-01-25', 19, 'dnolazco06@gmail.com') 
+INSERT INTO Clientes VALUES ('Miguel', 'Hernandez', 'Ibarra', '1995-10-12', 24, 'mhernandez@gmail.com') 
+INSERT INTO Clientes VALUES ('Andrea Dayane', 'Gomez', 'Palacios', '1998-06-22', 22, 'andy@gmail.com') 
+INSERT INTO Clientes VALUES ('Carlos Andre', 'Carrea', 'Faz', '1995-10-12', 24, 'andrecar@gmail.com') 
+INSERT INTO Clientes VALUES ('Mariana', 'Rodriguez', 'Castro', '1998-06-22', 22, 'mardz@gmail.com') 
+INSERT INTO Clientes VALUES ('Mariano', 'Castañeda', 'Parra', '1995-08-04', 24, 'cast@gmail.com') 
+INSERT INTO Clientes VALUES ('Veronica', 'Garza', 'Robles', '1997-06-25', 22, 'gvero@gmail.com') 
+INSERT INTO Clientes VALUES ('Daniel Emiliano', 'Saenz', 'Lozano', '1998-06-22', 22,'demuir@gmail.com') 
+INSERT INTO Clientes VALUES ('Sofia Elizabeth', 'Garza', 'Rangel', '2000-04-13', 20, 'sofgarza@gmail.com')
+INSERT INTO Clientes VALUES ('Adrian Alejandro', 'Reyes', 'Parra', '1999-02-13', 21, 'arp@gmail.com') 
+
+-- INSERT  Proveedores
+INSERT INTO Proveedores VALUES ('CDM150829H73', 'Civica Digital Mexico', 'Olimpo', 408, 'Oficina 20', 'Mas Palomas', '66463', 'Nuevo Leon', 'Mexico') 
+INSERT INTO Proveedores VALUES ('PAQ591107QY3', 'PAQUIN, S.A. DE C.V', 'NETZAHUALCOYOTL', 202, 'Local 10', 'Mirasur','98050','ZACATECAS', 'Mexico') 
+INSERT INTO Proveedores VALUES ('OPA880220626', 'OFICENTRO PAPELERÍA, S. A. DE C.V', 'MINA SIRENA', 97, 'Bodega 3','Minera','98040','Tamaulipas', 'Mexico') 
+INSERT INTO Proveedores VALUES ('MNL990623QPA', 'MUEBLES NOVA LUXE, S.A. DE C.V', 'LÓPEZ PORTILLO', 9, 'Bodega 20','LOMAS DEL CONSUELO','98608','ZACATECAS', 'Mexico') 
+INSERT INTO Proveedores VALUES ('COM940110JE2', 'COMPULOGIC, S.A. DE C.V.', 'AMARO', 122, 'Oficina 3','BOULEVARES','46283','Nuevo Leon', 'Mexico') 
+INSERT INTO Proveedores VALUES ('CLO400315I71 ', 'CASA LOPEZ S.A. DE C.V.', 'BLVD. LOPEZ PORTILLO ', 230, 'Oficina 21','GUADALUPE','98600','ZACATECAS', 'Mexico') 
+INSERT INTO Proveedores VALUES ('ENZ081008UG6', 'ENZAC S.A. DE C.V', 'HEROES DE CHAPULTEPEC', 635, 'Oficina N3','LOMAS','66982','Nuevo Leon', 'Mexico') 
+INSERT INTO Proveedores VALUES ('940209PH5SAI', 'SERVICIOS AMBIENTALES INTERNACIONALES,S.A DE R.L.D', 'PROL. PINO SUAREZ', 12, '2719-B','BELLA VISTA','55540','Nuevo Leon', 'Mexico') 
+INSERT INTO Proveedores VALUES ('ACA0002161J2', 'ALTA CARGA,S.A. DE C.V', 'CARR.TAMPICO-MANTE', 14, 'Bodega K32','CALETA','55010','Campeche', 'Mexico') 
+INSERT INTO Proveedores VALUES ('TRA740921HG1', 'TRANSINERGIA, S.A. DE C.V', 'ISLA BRITANICA', 3420, 'Oficina 3','Jardines del Sur','44950','Jalisco', 'Mexico') 
+
+-- INSERT Producto
+INSERT INTO Producto VALUES ('Suero Base Artistry', 'Belleza', 2, 896)
+INSERT INTO Producto VALUES ('Exact Fit Corrector líquido', 'Belleza', 2, 562)
+INSERT INTO Producto VALUES ('Cerocarb', 'Nutricion', 7, 264)
+INSERT INTO Producto VALUES ('Ajo Concentrado', 'Nutricion', 7, 460)
+INSERT INTO Producto VALUES ('Desodorante Ertia', 'Cuidado Personal', 10, 122)
+INSERT INTO Producto VALUES ('Shampoo Anticaída', 'Cuidado Personal', 10, 150)
+INSERT INTO Producto VALUES (' Limpiador concentrado multiusos', 'Hogar', 6, 225)
+INSERT INTO Producto VALUES ('Blanqueador para múltiples telas', 'Hogar', 6, 331)
+INSERT INTO Producto VALUES ('Jardinera Arcor', 'Conservas', 8, 30)
+INSERT INTO Producto VALUES ('Galletitas Traviata', 'Conservas', 2, 120)
+
+-- INSERT ALMACENES
+INSERT INTO Almacenes VALUES ('Birriesca', 'Cañada', '230', 'Bodeaga 2', 'Torres de Parra', '660131', 'Nuevo Leon', 'Mexico')
+INSERT INTO Almacenes VALUES ('Torrada', 'Monte Azul', '22', 'Bodega N78', 'Mirador del Sol', '66902', 'Nuevo Leon', 'Mexico')
+INSERT INTO Almacenes VALUES ('Amanayne', 'Gonzalitos', '321', 'Local K0', 'San Patricio', '90123', 'Guanajuato', 'Mexico')
+INSERT INTO Almacenes VALUES ('Galada', 'Sierra blanca', '901', 'Bodega 81', 'Jardines de Parra', '99562', 'Guanajuato', 'Mexico')
+INSERT INTO Almacenes VALUES ('Amaranto', 'Kilimanjaro', '133', 'Bodega I5', 'Villa hermosa', '89364', 'Ciudad de Mexico', 'Mexico')
+INSERT INTO Almacenes VALUES ('Tanzania', 'Armani', '034', 'Bodega L02', 'Nave Cerrada', '90732', 'Ciudad de Mexico', 'Mexico')
+INSERT INTO Almacenes VALUES ('Granate', 'Amarillo', '923', 'Bodega 1', 'Azul cielo', '23784', 'Jalisco', 'Mexico')
+INSERT INTO Almacenes VALUES ('Miraste', 'Muralillo', '084', 'Local T5', 'Jardines de Anahuac', '23897', 'Jalisco', 'Mexico')
+INSERT INTO Almacenes VALUES ('Zaote', 'Cuahutemoc', '762', 'Bodega 02J', 'Miraventana', '12934', 'Sinaloa', 'Mexico')
+INSERT INTO Almacenes VALUES ('Yate', 'Villa Gast', '342', 'Unidad 9', 'Villa morada', '12073', 'Sinaloa', 'Mexico')
+
+-- INSERT PEDIDOS
+INSERT INTO Pedidos VALUES (1, 10, 1, 10, '2019-12-05')
+INSERT INTO Pedidos VALUES (2, 9, 1, 9, '2019-10-22')
+INSERT INTO Pedidos VALUES (3, 8, 3, 8, '2019-11-19')
+INSERT INTO Pedidos VALUES (4, 7, 3, 7, '2020-03-05')
+INSERT INTO Pedidos VALUES (5, 6, 6, 6, '2020-01-25')
+INSERT INTO Pedidos VALUES (6, 5, 6, 5, '2018-08-16')
+INSERT INTO Pedidos VALUES (7, 4, 2, 4, '2018-10-25')
+INSERT INTO Pedidos VALUES (8, 3, 2, 3, '2020-04-11')
+INSERT INTO Pedidos VALUES (9, 2, 4, 2, '2020-02-25')
+INSERT INTO Pedidos VALUES (10, 1, 10, 1, '2020-01-25')
+
+-- UPDATE STATEMENTS WITH WHERE --
+UPDATE Almacenes SET NumeroInt = 'Bodega KT5' 
+WHERE Colonia IN ('San Patricio', 'Jardines de Parra')
+
+UPDATE Producto SET Costo = 250
+WHERE Costo <300
+
+UPDATE Almacenes SET CodigoPostal = '99784', Colonia = 'San Patricio'
+WHERE Estado = 'Guanajuato'
+
+UPDATE Producto SET Proveedor = 10, Costo = 850 
+WHERE Proveedor BETWEEN 1 AND 3
+
+UPDATE Proveedores SET Estado = 'Nuevo Leon'  
+WHERE Estado <> 'Nuevo Leon'
+
+-- DELETE STATEMENTS
+DELETE FROM Clientes WHERE Edad <= 19
+DELETE FROM Pedidos WHERE ID_Proveedor = 2
+DELETE FROM Producto WHERE Categoria = 'Conservas'
+DELETE FROM Almacenes WHERE Pais <> 'Mexico'
+DELETE FROM Pedidos WHERE FechaPedido < '2020-01-01'
