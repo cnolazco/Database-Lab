a755c59a9f062988a6ae0443f102a0a411399246
Carolina Nolazco <56035491+cnolazco@users.noreply.github.com>
2020-06-05 10:32:01 -0500

    Update SQLScript_Practica6.sql

--- SQLScript_Practica6.sql
+++ SQLScript_Practica6.sql
@@ -80,6 +80,138 @@ ALTER DATABASE [LBD] SET QUERY_STORE = OFF
 GO
 USE [LBD]
 GO
+/****** Object:  Table [dbo].[Clientes]    Script Date: 05/06/2020 09:13:58 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Clientes](
+	[ID_Cliente] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](50) NOT NULL,
+	[PrimerApellido] [varchar](50) NOT NULL,
+	[SegundoApellido] [varchar](50) NOT NULL,
+	[NombreCompleto]  AS (((([Nombre]+' ')+[PrimerApellido])+' ')+[SegundoApellido]),
+	[FechaDeNacimiento] [datetime] NOT NULL,
+	[Edad] [int] NOT NULL,
+	[CorreoElectronico] [varchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID_Cliente] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Pedidos]    Script Date: 05/06/2020 09:13:58 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Pedidos](
+	[ID_Pedido] [int] IDENTITY(1,1) NOT NULL,
+	[ID_Cliente] [int] NOT NULL,
+	[ID_Producto] [int] NOT NULL,
+	[ID_Proveedor] [int] NOT NULL,
+	[ID_Almacen] [int] NOT NULL,
+	[FechaPedido] [date] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID_Pedido] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Producto]    Script Date: 05/06/2020 09:13:58 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Producto](
+	[ID_Producto] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](50) NOT NULL,
+	[Categoria] [varchar](50) NOT NULL,
+	[Proveedor] [int] NOT NULL,
+	[Costo] [float] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID_Producto] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Proveedores]    Script Date: 05/06/2020 09:13:58 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Proveedores](
+	[ID_Proveedor] [int] IDENTITY(1,1) NOT NULL,
+	[RFC] [varchar](13) NOT NULL,
+	[RazonSocial] [varchar](50) NOT NULL,
+	[Calle] [varchar](50) NOT NULL,
+	[NumeroExt] [varchar](10) NOT NULL,
+	[NumeroInt] [varchar](50) NOT NULL,
+	[Colonia] [varchar](50) NOT NULL,
+	[CodigoPostal] [varchar](50) NOT NULL,
+	[Estado] [varchar](50) NOT NULL,
+	[Pais] [varchar](50) NOT NULL,
+	[DireccionProveedor]  AS ((((((((((((([Calle]+' ')+[NumeroInt])+' ')+[NumeroExt])+' ')+[Colonia])+' ')+[CodigoPostal])+' ')+[Estado])+' ')+[Pais])+' '),
+PRIMARY KEY CLUSTERED 
+(
+}	[ID_Proveedor] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Almacenes]    Script Date: 05/06/2020 09:13:58 a. m. ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Almacenes](
+	[ID_Almacen] [int] IDENTITY(1,1) NOT NULL,
+	[Nombre] [varchar](50) NOT NULL,
+	[Calle] [varchar](50) NOT NULL,
+	[NumeroExt] [varchar](20) NOT NULL,
+	[NumeroInt] [varchar](50) NOT NULL,
+	[Colonia] [varchar](50) NOT NULL,
+	[CodigoPostal] [varchar](50) NOT NULL,
+	[Estado] [varchar](50) NOT NULL,
+	[Pais] [varchar](50) NOT NULL,
+	[DireccionAlmacen]  AS ((((((((((((([Calle]+' ')+[NumeroInt])+' ')+[NumeroExt])+' ')+[Colonia])+' ')+[CodigoPostal])+' ')+[Estado])+' ')+[Pais])+' '),
+PRIMARY KEY CLUSTERED 
+(
+	[ID_Almacen] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Index [index_FechaEmisionPedidos]    Script Date: 05/06/2020 09:13:58 a. m. ******/
+CREATE NONCLUSTERED INDEX [index_FechaEmisionPedidos] ON [dbo].[Pedidos]
+(
+	[FechaPedido] DESC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, DROP_EXISTING = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+/****** Practica 3:  Add to the sql script backup at least 5 relations ******/
+ALTER TABLE [dbo].[Pedidos]  WITH CHECK ADD  CONSTRAINT [FK_AlmacenOrder] FOREIGN KEY([ID_Almacen])
+REFERENCES [dbo].[Almacenes] ([ID_Almacen])
+GO
+ALTER TABLE [dbo].[Pedidos] CHECK CONSTRAINT [FK_AlmacenOrder]
+GO
+ALTER TABLE [dbo].[Pedidos]  WITH CHECK ADD  CONSTRAINT [FK_ClienteOrder] FOREIGN KEY([ID_Cliente])
+REFERENCES [dbo].[Clientes] ([ID_Cliente])
+GO
+ALTER TABLE [dbo].[Pedidos] CHECK CONSTRAINT [FK_ClienteOrder]
+GO
+ALTER TABLE [dbo].[Pedidos]  WITH CHECK ADD  CONSTRAINT [FK_ProductOrder] FOREIGN KEY([ID_Producto])
+REFERENCES [dbo].[Producto] ([ID_Producto])
+GO
+ALTER TABLE [dbo].[Pedidos] CHECK CONSTRAINT [FK_ProductOrder]
+GO
+ALTER TABLE [dbo].[Producto]  WITH CHECK ADD  CONSTRAINT [FK_ProvedorOrder] FOREIGN KEY([Proveedor])
+REFERENCES [dbo].[Proveedores] ([ID_Proveedor])
+GO
+ALTER TABLE [dbo].[Producto] CHECK CONSTRAINT [FK_ProvedorOrder]
+GO
+ALTER TABLE [dbo].[Clientes]  WITH CHECK ADD  CONSTRAINT [CHK_Edad] CHECK  (([Edad]>=(18)))
+GO
+ALTER TABLE [dbo].[Clientes] CHECK CONSTRAINT [CHK_Edad]
+GO
+/****** Practica 6:  Add to the sql script backup the complete data sctructure and append: ******/
 SET IDENTITY_INSERT [dbo].[Almacenes] ON 
 
 INSERT [dbo].[Almacenes] ([ID_Almacen], [Nombre], [Calle], [NumeroExt], [NumeroInt], [Colonia], [CodigoPostal], [Estado], [Pais]) VALUES (1, N'Birriesca', N'Cañada', N'230', N'Bodeaga 2', N'Torres de Parra', N'660131', N'Nuevo Leon', N'Mexico')
